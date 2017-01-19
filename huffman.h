604000eb8fde19d2ba18da80c8acccc2a9b726aa
Ronak Chauhan <ronakchauhan97@gmail.com>
2017-01-19 20:23:49 +0530

    minor fixes and improved documentation

--- huffman.h
+++ huffman.h
@@ -3,32 +3,59 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
-/* Reads the input file and makes the frequency table */
+/* exit codes used
+ * 1 -> file not found
+ * 2 -> invalid file
+ * 3 -> empty file
+ */
+
+/* Read the input file and make the frequency table */
 size_t read(const char *source, uint32_t *table) {
 	size_t bytes_read = 0;
 	FILE *fin = fopen(source, "rb");
-	if(fin == NULL) {
+	
+	if(!fin) {
 		fprintf(stderr,"error: %s not found\n", source);
 		exit(1);
 	}
 
-	printf("Reading %s...\n", source);
-	uint8_t buff;
-	while(!feof(fin)) {
-		if(fread(&buff, sizeof(buff), 1, fin)) {
-			table[(uint8_t)(buff)]++;
-			bytes_read++;
-		}
-	}
+	fseek(fin, 0, SEEK_END);
+	long int size = ftell(fin);
 
-	fclose(fin);
-	return bytes_read;
+	if(size == 0){
+		fclose(fin);
+		fprintf(stderr, "error: %s is an empty file\n", source);
+		exit(3);
+	}
+	else {
+		fseek(fin, 0, SEEK_SET);
+		printf("Reading %s...\n", source);
+		uint8_t buff;
+		while(!feof(fin)) {
+			if(fread(&buff, sizeof(buff), 1, fin)) {
+				table[(uint8_t)(buff)]++;
+				bytes_read++;
+			}
+		}
+		fclose(fin);
+		return bytes_read;
+	}
 }
 
 
-/* Writes first char (i.e first 8 bits) of the encoded bitstring at the beginning of the compressed file.
+
+/* Write first char (i.e first 8 bits) of the encoded bitstring at the beginning of the compressed file.
  * Before starting decompression, these the chars are compared (see check() function) , if they donot match,
- * it implies that the file is not a hcomp archive
+ * it implies that the file is not a hcomp archive.
+ *
+ * It's a heuristic and in an in some rare case, the chars may match and hcomp may produce some
+ * garbage output file or a lead to a segmentation fault while running decompression routines.
+ * It's rare because here's how works:
+ *   - 1st byte -> read to check_num
+ *   - the next 2 bytes -> read to c
+ *   - the next [c*(1 + 4) + 4] bytes are skipped  (this value is depends on c)
+ *   - then 1 byte is read to num
+ *   - compare check_num and num
  */
 size_t write_check_num(const char *source, const char* target, char code[][256]) {
 	FILE *fin = fopen(source, "rb");
@@ -50,8 +77,14 @@ size_t write_check_num(const char *source, const char* target, char code[][256])
 				if(ors == 8) {
 					break;
 				}
-				else check_num = check_num << 1;
+				else {
+					check_num = check_num << 1;
+				}
 			}
+		}
+		else {
+			check_num = check_num << 8 - ors;
+			break;
 		}
 	}
 	
@@ -63,32 +96,46 @@ size_t write_check_num(const char *source, const char* target, char code[][256])
 
 
 
-bool check(const char* source){
+void check(const char* source){
 	FILE *fin = fopen(source, "rb");
-	if(fin == false) {
-		fprintf(stderr,"error: %s not found\n", source);
+	if(!fin) {
+		fprintf(stderr, "error: %s not found\n", source);
 		exit(1);
 	}
 
-	uint8_t check_num;
-	uint8_t num;
-	uint16_t c;
+	fseek(fin, 0, SEEK_END);
+	long int size = ftell(fin);
 
-	fread(&check_num, sizeof(check_num), 1, fin);
-	fread(&c, sizeof(c), 1, fin);
+	if(size == 0){
+		fclose(fin);
+		fprintf(stderr, "error: %s is an empty file\n", source);
+		exit(3);
+	}
+	else {
+		fseek(fin, 0, SEEK_SET);
+		uint8_t check_num = 0;
+		uint8_t num = 1;
+		uint16_t c;
 
-	fseek(fin, c*(sizeof(uint8_t) + sizeof(uint32_t)) + sizeof(uint32_t), SEEK_CUR);
-	fread(&num, sizeof(num), 1, fin);
-	fclose(fin);
+		fread(&check_num, sizeof(check_num), 1, fin);
+		fread(&c, sizeof(c), 1, fin);
 
-	return (num == check_num) ? true : false;
+		fseek(fin, c*(sizeof(uint8_t) + sizeof(uint32_t)) + sizeof(uint32_t), SEEK_CUR);
+		fread(&num, sizeof(num), 1, fin);
+		fclose(fin);
+
+		if(num != check_num) {
+			fprintf(stderr, "error: %s is not a hcomp archive\n", source);
+			exit(2);
+		}
+	}
 }
 
 
 
-/* Writes chars and their frequencies to compressed file so that the tree can
+/* Write chars and their frequencies to compressed file so that the tree can
  * be rebuilt during decompression.
- * This is called before the huffman code is written
+ * This is called before the compressed bitstring is written
  */
 size_t write_header_info(const char* target, uint32_t* table, uint32_t uncomp_bytes) {
 	FILE* fout = fopen(target, "ab");
@@ -126,10 +173,10 @@ size_t write_header_info(const char* target, uint32_t* table, uint32_t uncomp_by
 
 
 
-/* Reads frequency table from compressed file for rebuilding the huffman tree */
+/* Read frequency table from compressed file for rebuilding the huffman tree */
 size_t parse_header_info(const char* source, uint32_t *table){
 	FILE* fin = fopen(source, "rb");
-	if(fin == NULL) {
+	if(!fin) {
 		fprintf(stderr,"error: %s not found\n", source);
 		exit(1);
 	}
@@ -156,12 +203,12 @@ size_t parse_header_info(const char* source, uint32_t *table){
 
 
 
-/* Builds tree using nodes from the list of pending nodes.
+/* Build tree using nodes from the list of pending nodes.
  * This is done till there is only one node left in the pending list (i.e a single tree)
  */
 t_node *build_tree(l_node **t) {
 	l_node *w_list = *t;
-	t_node *root = NULL;
+	t_node *root = w_list->subtree;
 	t_node *min1 = NULL, *min2 = NULL;
 	uint32_t min_val = 0;
 
@@ -184,7 +231,7 @@ t_node *build_tree(l_node **t) {
 
 
 
-/* Traverses the tree to generate huffman code and store it in a string
+/* Traverse the tree to generate huffman code and store it in a string
  * at corresponding ascii char valued index
  */
 void get_code(t_node *t, char *current_code, char hcode[][256]) {
@@ -210,9 +257,9 @@ void get_code(t_node *t, char *current_code, char hcode[][256]) {
 
 
 
-/* Writes the source file to target using corresponding huffman codes instead of ascii codes.
- * Starts by reading the source file again, fills up current_buff(an unsigned char) with huffman code
- * and writes it to the target file.
+/* Write the source file to target using corresponding huffman codes instead of ascii codes.
+ * Start by reading the source file again, fill up current_buff(an unsigned char) with huffman code
+ * and write it to the target file.
  */
 size_t compress(const char *source, const char *target, uint32_t n_bytes, char code[][256]) {
 	FILE *fin = fopen(source, "rb");
@@ -277,21 +324,21 @@ size_t compress(const char *source, const char *target, uint32_t n_bytes, char c
 /* Traverse the tree while parsing huffman codes, and as a leaf node is reached, 
  * write the corresponding char to target file.
  * As 8 bits are read at a time, bit manipulations are used to differentiate 1s from 0s.
- * The additional arguement b is just used for counting the no. of bytes read
+ * The additional arguement *b is just used for counting the no. of bytes read
  */
 size_t decompress(const char *source, const char *target, t_node* root, size_t *b) {
 	FILE *fin = fopen(source, "rb");
 	FILE *fout = fopen(target, "wb");
 
-	/* First reading #leaf_nodes (2 bytes), and the skipping the #leaf_nodes entries if the frequency table 
+	/* First reading #leaf_nodes (2 bytes), and the skipping the #leaf_nodes entries of the frequency table 
 	 * as they are already read, and the tree is ready
 	 * Then, reading the original(uncompressed) file size so that the extra padding of 0s doesn't 
-	 * lead to writing extra bytes
+	 * lead to writing extra bytes after decompression
 	 * (original file size was written to file in write_header_info())
 	 */
 	uint16_t c = 0; /* #leaf_nodes */
 	uint32_t uncomp_bytes = 0;
-	fseek(fin, sizeof(uint8_t), SEEK_CUR);
+	fseek(fin, sizeof(uint8_t), SEEK_SET);
 	fread(&c, sizeof(c), 1, fin);
 	fseek(fin, c*(sizeof(uint8_t) + sizeof(uint32_t)), SEEK_CUR); 
 	fread(&uncomp_bytes, sizeof(uncomp_bytes), 1, fin);
@@ -308,7 +355,7 @@ size_t decompress(const char *source, const char *target, t_node* root, size_t *
 	uint8_t current_char = 0;  /* char read from fin (source) */
 	uint8_t check = 0; /* used to check bits in current_char */
 
-	printf("decoding huffman code and writing %s...\n", target);
+	printf("decoding huffman codes and writing %s...\n", target);
 	t_node *current = root;
 	while(feof(fin) == false && fread(&current_char, sizeof(current_char), 1, fin) == true) {
 		for(int8_t i = 7; i >= 0; i--) {
